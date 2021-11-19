cd01825d7316e4fba55c56d7b01b371fcb667cf7
Siddharth Roy <siddharthroy36912@gmail.com>
2021-11-19 09:52:50 +0530

    Added mobile view navigation

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
 // Elements
 var headerEl;
+var menuBtnEl;
+var menuEl;
+var isMenuToggled = false;
 
 function getElements() {
 	headerEl = document.getElementsByTagName("header")[0];
+	menuBtnEl = document.getElementsByClassName("menu-btn")[0];
+	menuEl = document.getElementsByClassName("header-nav-list")[0];
+}
+
+function toggleMenu() {
+	if (!isMenuToggled) {
+		menuEl.classList.add("header-nav-list-expand");
+	} else {
+		menuEl.classList.remove("header-nav-list-expand");
+	}
+
+	isMenuToggled = !isMenuToggled;
 }
 
 function onScroll() {
@@ -18,6 +33,9 @@ function onload() {
 
 	headerEl.classList.add("header-expand");
 	addEventListener("scroll", onScroll);
+
+	menuBtnEl.addEventListener("click", toggleMenu);
+	menuEl.addEventListener("click", toggleMenu);
 }
 
 window.onload(onload);
