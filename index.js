d1b75592ec832b98eecc6149fb4d8e940fcb7cec
Siddharth Roy <siddharthroy36912@gmail.com>
2021-11-19 09:22:14 +0530

    Added animated header

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-console.log("Hello World")
+// Elements
+var headerEl;
+
+function getElements() {
+	headerEl = document.getElementsByTagName("header")[0];
+}
+
+function onScroll() {
+	if (document.body.scrollTop > 5 || document.documentElement.scrollTop > 5) {
+		headerEl.classList.remove("header-expand");
+	} else {
+		headerEl.classList.add("header-expand");
+	}
+}
+
+function onload() {
+	getElements();
+
+	headerEl.classList.add("header-expand");
+	addEventListener("scroll", onScroll);
+}
+
+window.onload(onload);
+
